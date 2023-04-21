7668c9b343e9e5e34075ce9c9e04da1dbf32eb94
Ufuk ACIKGOZ <ufuk.acikgoz@hotmail.com>
2023-04-21 14:58:21 +0300

    mapLeafletFirts

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("test");
+var map = L.map("map", {
+  center: [38.45186951421481, 27.144738932596645],
+  zoom: 13,
+});
+
+L.tileLayer("http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}", {
+  maxZoom: 20,
+  subdomains: ["mt0", "mt1", "mt2", "mt3"],
+}).addTo(map);
