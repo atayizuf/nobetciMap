26e8ef15d452be7ddbdaa788d3e569aa6e88765d
Ufuk ACIKGOZ <ufuk.acikgoz@hotmail.com>
2023-05-21 23:37:40 +0300

    duzeltmelerVeHaritaMerkezleme

--- script.js
+++ script.js
@@ -1,54 +1,42 @@
 var map = L.map("map", {
   center: [38.451440213829905, 27.141684430822952], //38.451440213829905, 27.141684430822952
-  zoom: 13,
+  zoom: 11,
 });
-
+const nobEczaneLink = "https://openapi.izmir.bel.tr/api/ibb/nobetcieczaneler";
 L.tileLayer("http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}", {
   maxZoom: 20,
   subdomains: ["mt0", "mt1", "mt2", "mt3"],
   attribution:
     'maked by <a href="https://github.com/atayizuf/nobetciMap/" target="_blank">Ufuk Açıkgöz</a>',
 }).addTo(map);
-
-let Adi = [];
-let Adres = [];
-let Bolge = [];
-let BolgeAciklama = [];
-let LokasyonX = [];
-let LokasyonY = [];
-let Telefon = [];
-
 const myIcon = L.icon({
   iconUrl: "test.png",
   iconSize: [50, 70],
   popupAnchor: [2, -35],
 });
-
-fetch("https://openapi.izmir.bel.tr/api/ibb/nobetcieczaneler")
+fetch(nobEczaneLink)
   .then((resp) => resp.json())
   .then((resp) => {
-    resp.forEach((x) => {
-      Adi.push(x.Adi);
-      Adres.push(x.Adres);
-      Bolge.push(x.Bolge);
-      BolgeAciklama.push(x.BolgeAciklama);
-      LokasyonX.push(x.LokasyonX);
-      LokasyonY.push(x.LokasyonY);
-      Telefon.push(x.Telefon);
-    });
-    for (let i = 0; i < Bolge.length; i++) {
-      const marker = L.marker([LokasyonX[i], LokasyonY[i]], {
-        icon: myIcon,
-      })
+    for (let i = 0; i < resp.length; i++) {
+      const marker = L.marker(
+        [parseFloat(resp[i].LokasyonX), parseFloat(resp[i].LokasyonY)],
+        {
+          icon: myIcon,
+        }
+      )
         .addTo(map)
         .bindPopup(
-          `<div class="ana"><h4 id="popid">${Bolge[i]}</h4><h2>${Adi[i]}</h2><br /><a class="telefon" href="tel:${Telefon[i]}">${Telefon[i]}</a><br><div>${BolgeAciklama[i]}</div><a class="gitbuton" href="https://www.google.com/maps/place/${LokasyonX[i]},${LokasyonY[i]}/@${LokasyonX[i]},${LokasyonY[i]},17.25z" target="_blank">Navigasyon Kullan</a></div>`
+          `<div class="ana"><h4 id="popid">${resp[i].Bolge}</h4><h2>${resp[i].Adi}</h2><br /><a class="telefon" href="tel:${resp[i].Telefon}">${resp[i].Telefon}</a><br><div id="aciklama">${resp[i].BolgeAciklama}</div><a class="gitbuton" href="https://www.google.com/maps/place/${resp[i].LokasyonX},${resp[i].LokasyonY}/@${resp[i].LokasyonX},${resp[i].LokasyonY},17.25z" target="_blank">Navigasyon Kullan</a></div>`
         );
     }
   });
-
 const kapatbas = document.getElementById("kapat");
 const basonerikapat = document.getElementById("basoneri");
 kapatbas.addEventListener("click", () => {
   basonerikapat.style.display = "none";
+});
+
+const viewSet = document.getElementById("setview");
+viewSet.addEventListener("click", () => {
+  map.setView(new L.LatLng(38.451440213829905, 27.141684430822952), 11);
 });
